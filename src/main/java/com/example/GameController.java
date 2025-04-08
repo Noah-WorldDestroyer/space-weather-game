ae52c4c290c2b00b22c1d9cbe10d8fe8618aba22
Noah-DestroyerOfWorlds <noahfosterbrown@gmail.com>
2025-04-08 10:02:52 -0400

    08 April 2025

--- src/main/java/com/example/GameController.java
+++ src/main/java/com/example/GameController.java
@@ -2,11 +2,21 @@ package com.example;
 
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
 
 public class GameController {
 
     @FXML
     private ImageView alienCharacterImageView;
 
-    
+    @FXML
+    private Rectangle greyBackgroundRectangle;
+
+
+
+
+    @FXML
+    void initialize(){
+        greyBackgroundRectangle.setOpacity(0.5);
+    }
 }
